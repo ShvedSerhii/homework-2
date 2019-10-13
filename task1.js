f14335143d713ab4b0a0ce2afc37980eb6935e8c
Serhii Shved <chuhaystyr@gmail.com>
2019-10-14 01:35:58 +0300

    tasc

---
+++ task1.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function myMinMaxSumFunction (array) {
+    let result = {};
+    array = array.filter(item => !isNaN(+item));
+    result.sum = array.reduce((sum, current) => sum + current, 0);
+    array.sort( (a, b) => a - b );
+    result.min = array[0];
+    result.max = array.pop();
+    return result;
+}
