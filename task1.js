8ccf96bf5d5194fc0ad7d6abbc3e3ecbc3c8c882
Serhii Shved <chuhaystyr@gmail.com>
2019-10-14 20:03:48 +0300

    task1 and task2 completed

--- task1.js
+++ task1.js
@@ -1,11 +1,11 @@
 "use strict";
 
-function mySumMinMaxFunction(array) {
+function mySumMinMaxFunction(arr) {
   let result = {};
-  array = array.filter(item => !isNaN(+item));
-  result.sum = array.reduce((sum, current) => sum + current, 0);
-  array.sort((a, b) => a - b);
-  result.min = array[0];
-  result.max = array.pop();
+  arr = arr.filter(item => !isNaN(+item));
+  result.sum = arr.reduce((sum, current) => sum + current, 0);
+  arr.sort((a, b) => a - b);
+  result.min = arr[0];
+  result.max = arr.pop();
   return result;
 }
---
+++ task2.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function waterCalc(arr) {
+  const n = arr.length;
+
+  let directArr = new Array(n);
+  let reverseArr = new Array(n);
+  let water = 0;
+
+  directArr[0] = arr[0];
+  for (let i = 1; i < n; ++i) {
+    directArr[i] = Math.max(directArr[i - 1], arr[i]);
+  }
+
+  reverseArr[n - 1] = arr[n - 1];
+  for (let i = n - 2; i >= 0; --i) {
+    reverseArr[i] = Math.max(reverseArr[i + 1], arr[i]);
+  }
+
+  arr.forEach((item, index) => {
+    water += Math.min(directArr[index], reverseArr[index]) - item;
+  });
+
+  return water;
+}
