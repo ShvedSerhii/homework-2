0cafe4542c948a3d56174de9df8320e55f91f876
Serhii Shved <chuhaystyr@gmail.com>
2019-10-22 22:27:00 +0300

    fix

--- task3.js
+++ task3.js
@@ -1,24 +1,26 @@
-"use strict";
+"use strict"
 
 function makeChange(price, pay) {
-  const stackBanknotes = [100, 50, 20, 10, 5, 2, 1];
+  const stackBanknotes = [100, 50, 20, 10, 5, 2, 1]
 
-  let change = pay - price;
-  let index = [];
-  let result = [];
+  if (pay < price) throw new Error("Not enough money to buy!")
+
+  let change = pay - price
+  let index = []
+  let result = []
 
   stackBanknotes.forEach(item => {
-    let changeAmount = Math.floor(change / item);
-    change -= item * changeAmount;
-    index.push(changeAmount);
-  });
+    let changeAmount = Math.floor(change / item)
+    change -= item * changeAmount
+    index.push(changeAmount)
+  })
 
   for (let i = 0; i < index.length; ++i) {
-    if (index[i] == 0) continue;
+    if (index[i] == 0) continue
     for (let j = index[i]; j > 0; --j) {
-      result.push(stackBanknotes[i]);
+      result.push(stackBanknotes[i])
     }
   }
 
-  return result;
+  return result
 }
